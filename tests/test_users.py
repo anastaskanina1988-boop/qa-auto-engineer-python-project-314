ccd08bf14d1c531fb6b490d21ed99280d8452d9b
Anastasia Kanina <anastaskanina1988@gmail.com>
2026-06-16 16:04:16 +0300

    add users tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,10 +1,10 @@
+
+import uuid
+from selenium.webdriver.common.by import By
 try:
     from pages import LoginPage
 except ImportError:
     from .pages import LoginPage
-
-from selenium.webdriver.common.by import By
-
 
 def test_users_table_loaded(driver, app_base_url):
     driver.get(app_base_url)
@@ -17,3 +17,40 @@ def test_users_table_loaded(driver, app_base_url):
     assert driver.find_element(By.XPATH, "//th[contains(., 'Email')]")
     assert driver.find_element(By.XPATH, "//th[contains(., 'First name')]")
     assert driver.find_element(By.XPATH, "//th[contains(., 'Last name')]")
+
+def test_create_user(driver, app_base_url):
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+
+    driver.get(app_base_url)
+
+    login_page = LoginPage(driver)
+    login_page.login("test", "test")
+
+    driver.find_element(By.LINK_TEXT, "Users").click()
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located(
+            (By.XPATH, "//th[contains(., 'Email')]")
+        )
+    )
+
+    driver.find_element(
+        By.XPATH,
+        '//*[@id="main-content"]/div/div/div[1]/div/a'
+    ).click()
+
+    email = f"{uuid.uuid4().hex[:8]}@example.com"
+
+    inputs = driver.find_elements(By.TAG_NAME, "input")
+
+    inputs[0].send_keys(email)
+    inputs[1].send_keys("Test")
+    inputs[2].send_keys("User")
+
+    driver.find_element(
+        By.CSS_SELECTOR,
+        'button[aria-label="Save"]'
+    ).click()
+
+    assert email in driver.page_source
