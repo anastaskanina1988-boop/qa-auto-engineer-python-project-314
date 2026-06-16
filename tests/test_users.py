b6cc1bea36055ed79f7e4c0fad379e12d3d87624
Anastasia Kanina <anastaskanina1988@gmail.com>
2026-06-16 16:23:42 +0300

    add users CRUD tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -106,4 +106,45 @@ def test_edit_user(driver, app_base_url):
         '//button[@aria-label="Save"]'
     ).click()
 
-    assert "Updated" in driver.page_source    
+    assert "Updated" in driver.page_source  
+
+def test_delete_user(driver, app_base_url):
+    driver.get(app_base_url)
+
+    login_page = LoginPage(driver)
+    login_page.login("test", "test")
+
+    driver.find_element(By.LINK_TEXT, "Users").click()
+
+    driver.find_element(
+        By.XPATH,
+        '//*[@id="main-content"]/div/div/div[1]/div/a'
+    ).click()
+
+    email = f"{uuid.uuid4().hex[:8]}@example.com"
+
+    inputs = driver.find_elements(By.TAG_NAME, "input")
+
+    inputs[0].send_keys(email)
+    inputs[1].send_keys("Delete")
+    inputs[2].send_keys("User")
+
+    driver.find_element(
+        By.CSS_SELECTOR,
+        'button[aria-label="Save"]'
+    ).click()
+
+    assert email in driver.page_source
+
+    driver.find_element(
+        By.XPATH,
+        f"//span[contains(text(), '{email}')]"
+    ).click()
+
+    driver.find_element(
+        By.CSS_SELECTOR,
+        'button[aria-label="Delete"]'
+    ).click()
+    
+    driver.find_element(By.LINK_TEXT, "Users").click()
+    assert email not in driver.page_source      
